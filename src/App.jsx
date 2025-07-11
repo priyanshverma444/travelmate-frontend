85f5e4bd84a4c79efa5906ab2a22edde4085d8de
Priyansh Verma <verma.priyansh444@gmail.com>
2025-07-12 01:20:03 +0530

    feat(trips): add trips route to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import TripList from "./pages/TripList";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/trips" element={<TripList />} />
       </Routes>
     </Router>
   );
