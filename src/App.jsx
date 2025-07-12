4eb5d137534f27b8fe9af71963ef22867fb9d611
Priyansh Verma <verma.priyansh444@gmail.com>
2025-07-13 01:27:16 +0530

    chore(routes): add homepage route to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import TripList from "./pages/TripList";
 import TripForm from "./pages/TripForm";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/trips" element={<TripList />} />
