f5ced33729673ab9727eec7317a76002903e2993
Priyansh Verma <verma.priyansh444@gmail.com>
2025-07-14 00:02:28 +0530

    chore(routes): register reset and forgot password pages in routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,21 @@
-import { useEffect } from "react";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
-  useNavigate,
+  Navigate,
 } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import TripList from "./pages/TripList";
 import TripForm from "./pages/TripForm";
 import Home from "./pages/Home";
-import Layout from "./components/Layout";
-import { useAuth } from "./context/AuthContext";
 import NotFound from "./pages/NotFound";
+import Layout from "./components/Layout";
+import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
+import ProtectedRoute from "./components/ProtectedRoute";
 
-// Wrapper for redirection on logout
 function AppRoutes() {
-  const { token } = useAuth();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (!token) {
-      navigate("/login");
-    }
-  }, [token]);
 
   return (
     <Routes>
@@ -31,9 +23,34 @@ function AppRoutes() {
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/trips" element={<TripList />} />
-        <Route path="/trips/new" element={<TripForm />} />
-        <Route path="/trips/edit/:id" element={<TripForm />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+
+        {/* Protected Routes */}
+        <Route
+          path="/trips"
+          element={
+            <ProtectedRoute>
+              <TripList />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/trips/new"
+          element={
+            <ProtectedRoute>
+              <TripForm />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/trips/edit/:id"
+          element={
+            <ProtectedRoute>
+              <TripForm />
+            </ProtectedRoute>
+          }
+        />
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
